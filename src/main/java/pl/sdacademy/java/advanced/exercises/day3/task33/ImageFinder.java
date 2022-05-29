45fb01fd26499b468abef606023912343f5df75b
Pawel Dudko <paweldudko@outlook.com>
2022-05-29 14:06:30 +0200

    solution for task33

--- src/main/java/pl/sdacademy/java/advanced/exercises/day3/task33/ImageFinder.java
+++ src/main/java/pl/sdacademy/java/advanced/exercises/day3/task33/ImageFinder.java
@@ -1,10 +1,12 @@
 package pl.sdacademy.java.advanced.exercises.day3.task33;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ImageFinder {
@@ -12,7 +14,12 @@ public class ImageFinder {
         //założenie: plik ze zdjęciem to plik z rozszerzeniem *.png lub *.jpeg
         //Files.walk(path) -> przechodzi po drzewie folderów, zaczynając od directory
         try (Stream<Path> walk = Files.walk(directory)){
-            //todo logic
+            return walk
+                    //.map(Path::toFile)
+                    //.map(File::getName)
+                    .map(p -> p.getFileName().toString())
+                    .filter(i -> i.endsWith(".png") || i.endsWith(".jpeg"))
+                    .collect(Collectors.toList());
         } catch(IOException e) {
             System.out.println("Exception: " + e);
         }
